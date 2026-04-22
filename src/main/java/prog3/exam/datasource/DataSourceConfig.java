6334263485ecc45df72e3e6923db868eebbe9a65
Vaniaina <vanimendes344@gmail.com>
2026-04-22 11:20:25 +0300

    feat : created the feat-c-d branch

--- src/main/java/prog3/exam/datasource/DataSourceConfig.java
+++ src/main/java/prog3/exam/datasource/DataSourceConfig.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.github.cdimascio.dotenv.Dotenv;
