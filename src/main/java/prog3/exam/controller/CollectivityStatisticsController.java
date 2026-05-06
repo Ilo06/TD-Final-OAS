698fcbdb37f2f1d703c2cce89b347ece370abf95
Vaniaina <vanimendes344@gmail.com>
2026-05-06 12:56:18 +0300

    feat : integrated the service to the CollectivityStatisticsController for GET /collectivities/statistics

--- src/main/java/prog3/exam/controller/CollectivityStatisticsController.java
+++ src/main/java/prog3/exam/controller/CollectivityStatisticsController.java
@@ -3,7 +3,9 @@ package prog3.exam.controller;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import prog3.exam.model.CollectivityLocalStatistics;
+import prog3.exam.model.CollectivityOverallStatistics;
 import prog3.exam.service.CollectivityStatisticsService;
+import prog3.exam.service.FederationStatisticsService;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -11,10 +13,13 @@ import java.util.List;
 @RestController
 public class CollectivityStatisticsController {
 
-    private final CollectivityStatisticsService statisticsService;
+     private final CollectivityStatisticsService statisticsService;
+    private final FederationStatisticsService federationStatisticsService;
 
-    public CollectivityStatisticsController(CollectivityStatisticsService statisticsService) {
+    public CollectivityStatisticsController(CollectivityStatisticsService statisticsService,
+                                             FederationStatisticsService federationStatisticsService) {
         this.statisticsService = statisticsService;
+        this.federationStatisticsService = federationStatisticsService;
     }
 
     // TODO: typo here on collectivites
@@ -24,5 +29,12 @@ public class CollectivityStatisticsController {
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to) {
         return statisticsService.getStatistics(id, from, to);
+    }
+
+    @GetMapping("/collectivities/statistics")
+    public List<CollectivityOverallStatistics> getOverallStatistics(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to) {
+        return federationStatisticsService.getOverallStatistics(from, to);
     }
 }
